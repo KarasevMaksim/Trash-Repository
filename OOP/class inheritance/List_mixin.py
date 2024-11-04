54c28201b0e6ccd8cd28fcc186b3948ae41dc422
TheAllanProgramm <metro3216@gmail.com>
2024-11-04 06:03:56 +0300

    update list mixin

--- OOP/class inheritance/List_mixin.py
+++ OOP/class inheritance/List_mixin.py
@@ -9,7 +9,12 @@ class Loggable:
 class LoggableList(list, Loggable):
     def append(self, value):
         list.append(self, value)
+        # or super().append(value)
+        # or super(LoggableList, self).append(value)
         self.log(value)
+        # or super().log(value)
+        # or super(Loggable, self).log(value)
+        # or Loggable.log(self, value)
         
         
         
