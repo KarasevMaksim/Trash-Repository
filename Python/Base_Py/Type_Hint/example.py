4bad69f906feabab66cb9747bb50377d321d0695
TheAllanProgramm <metro3216@gmail.com>
2024-11-17 16:08:32 +0300

    update type hint examples

--- Python/Base_Py/Type_Hint/example.py
+++ Python/Base_Py/Type_Hint/example.py
@@ -1,6 +1,8 @@
 from typing import Final, Callable, Type, TypeVar
 
 
+# Base TypeHint ===============================================================
+
 PI: Final[float] = 3.14
 MAX_COUNT: Final[int] = 100
 
@@ -49,3 +51,84 @@ T = TypeVar('T', bound=FurFur) # TypeHint для одного класс и ег
     
 def constract_object(class_obj: Type[T]) -> T:
     return class_obj()
+
+
+# TypeHint with NamedTuple ====================================================
+from typing import NamedTuple
+
+class Coordinates(NamedTuple):
+    latitude: float
+    longitude: float
+    
+def get_gps_coord() -> Coordinates:
+    result = Coordinates(
+        latitude=10.0,
+        longitude=20.0
+    )
+    return result
+
+# TypeHint for Dicts with Literal =============================================
+from typing import Literal
+
+def get_gps_coord2() -> dict[Literal['latitude'] | Literal['longitude'], float]:
+    res = {
+        'latitude': 10.0,
+        'longitude': 20.0
+    }
+    return res
+
+# TypeHint with TypedDict =====================================================
+from typing import TypedDict
+
+class Coordinates2(TypedDict):
+    longitude: float
+    latitude: float
+    
+def get_gps_coord3() -> Coordinates2:
+    res = Coordinates2(
+        **{
+            'latitude': 10.1,
+            'longitude': 20.1
+          }
+    )
+    return res
+
+# TypeHint with @dataclass
+# Создает эмуляцию обращения по атрибутам как в экземплярах класс, в отличии
+# от именованных кортежей, является изменяемым типом данных.
+from dataclasses import dataclass
+
+@dataclass
+class Coordinates4:
+    longitude: float
+    latitude: float
+    
+def get_gps_coord4() -> Coordinates4:
+    res = Coordinates4(
+        **{
+            'latitude': 10.1,
+            'longitude': 20.1
+          }
+    )
+    # or
+    # res = Coordinates4(latitude=10.1, longitude=20.1)
+    return res
+
+# TypeHint for Enum ===========================================================
+# Используется для перечесления данных
+from enum import Enum
+from typing import NamedTuple
+from datetime import datetime
+
+
+class WeatherType(Enum):
+    THUNDERSTORM = 'Гроза'
+    RAIN = 'Дождь'
+    SNOW = 'Снег'
+    CLEAR = 'Ясно'
+
+class Weather(NamedTuple):
+    temerature: int
+    weather_type: WeatherType
+    city: str
+    sunrise: datetime
