0e730dbfa678acf48ec5733b488953b6b71ce5f9
TheAllanProgramm <metro3216@gmail.com>
2024-11-20 17:57:44 +0300

    update generator examples

--- Python/Base_Py/Iteration_and_Generation/Generators.py
+++ Python/Base_Py/Iteration_and_Generation/Generators.py
@@ -71,7 +71,106 @@ def get_item_from_list_gen(mass):
         # Рекурсивно продолжаем обход оставшихся элементов
         yield from get_item_from_list_gen(mass[1:])
 
-if __name__ == '__main__':
-    gen = get_item_from_list_gen(mass)
-    print(next(gen))
-    print(next(gen))
+# if __name__ == '__main__':
+#     gen = get_item_from_list_gen(mass)
+#     print(next(gen))
+#     print(next(gen))
+
+# Generators for read file ====================================================
+import os
+
+def update_gen() -> str:
+    for line in file_lines:
+        yield f'=^^= {line} UwU'
+
+os.chdir('.\\Python\\Base_Py\\Iteration_and_Generation')
+with open('file.txt', encoding='utf-8') as file:
+    file_lines = (line.strip() for line in file)
+    gen = update_gen()
+    
+    # print(next(gen))
+    # print(next(gen))
+    
+    
+# Прочие примеры ==============================================================
+
+from typing import NamedTuple, TypeVar, Iterator
+
+
+class Person(NamedTuple):
+    Name: str
+    Country: str
+    Gender: str
+    Rodilsy: int
+    Umir: int
+    
+T = TypeVar('T', bound=Person)
+
+persons: list = [Person('E. M. Ashe', 'American', 'male', 1867, 1941),
+           Person('Goran Aslin', 'Swedish', 'male', 1980, 0),
+           Person('Erik Gunnar Asplund', 'Swedish', 'male', 1885, 1940),
+           Person('Genevieve Asse', 'French', 'female', 1949, 0),
+           Person('Irene Adler', 'Swedish', 'female', 2005, 0),
+           Person('Sergio Asti', 'Italian', 'male', 1926, 0),
+           Person('Olof Backman', 'Swedish', 'male', 1999, 0),
+           Person('Alyson Hannigan', 'Swedish', 'female', 1940, 1987),
+           Person('Dana Atchley', 'American', 'female', 1941, 2000),
+           Person('Monika Andersson', 'Swedish', 'female', 1957, 0),
+           Person('Shura_Stone', 'Russian', 'male', 2000, 0),
+           Person('Jon Bale', 'Swedish', 'male', 1950, 0)]
+
+def filter_gen_country(person: list[T]) -> Iterator[T]:
+    '''Выдает значния, которые соответствуют условию'''
+    for i in person:
+        if i.Country == 'Swedish' and not i.Umir:
+            yield i
+            
+def filter_youngest(person: Iterator[T]) -> str:
+    result = max(person, key=lambda x: x.Rodilsy)
+    return result.Name
+        
+# if __name__ == '__main__':
+#     print(filter_youngest(filter_gen_country(persons)))
+
+def unique(iterable):
+    '''Выдает уникальные значения из итерируемой последовательности'''
+    dublikate = set()
+    for i in iterable:
+        if not i in dublikate:
+            dublikate.add(i)
+            yield i
+            
+# if __name__ == '__main__':
+#     iterator = iter('111222333') 
+#     uniques = unique(iterator) 
+#     print(next(uniques)) 
+#     print(next(uniques)) 
+#     print(next(uniques))
+
+numbers = [1,2,3,4,5]
+
+def around(iterable):
+    iterable = iter(iterable)
+    x = None
+    y = None
+    z = None
+    for i in iterable:
+        if not x and not y and not z:
+            y = i
+            z = next(iterable)
+            yield (x, y, z)
+        else:
+            x = y
+            y = z
+            z = i
+            yield (x, y, z)
+    else:
+        x = y
+        y = i
+        z = None
+        yield (x, y, z)
+
+# if __name__ == '__main__':
+#     print(*around(numbers)) 
+#     print(*around('hey'))
+            
